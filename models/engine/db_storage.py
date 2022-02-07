ad6c1de7c2dfd0ffcb533c300c4938a2ba81905d
Mahiuha <tiasho.mahiuha@gmail.com>
2022-02-07 19:09:02 +0300

    adding more tests on db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+        """
+        return_all = self.all(cls)
+        for value in return_all.values():
+            if value.id == id:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        return (len(self.all(cls)))
