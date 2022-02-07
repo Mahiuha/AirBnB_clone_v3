a4a13c38a7b99f42ae01e7c36ffcf17b4e3666a3
Mahiuha <tiasho.mahiuha@gmail.com>
2022-02-07 19:17:55 +0300

    adding more tests

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,7 @@ Contains the class DBStorage
 """
 
 import models
+import json
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -12,7 +13,6 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,6 +114,7 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
 
+
     @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_z00_count(self):
         """Test Returns the object based on the class and its ID,
